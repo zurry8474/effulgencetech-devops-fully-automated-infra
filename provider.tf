60f778535b46395295f079ef8ac79b6834bbe06f
Michaelgwei86 <Michaelgwei86@gmail.com>
2023-05-20 13:22:01 +0100

    modified

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "us-west-2"
-  profile = "default"
  // access_key              = "${env.AWS_ACCESS_KEY_ID}" 
  // secret_key              = "${env.AWS_SECRET_ACCESS_KEY}"
   # profile = "effulgencetech"
